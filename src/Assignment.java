064cc3965a83d7ef9e1eabf52d44aed2144f7c27
W. Mooncai <wm2tech@bigfoot.com>
2012-10-01 20:43:34 -0400

    Added Assignment.java

---
+++ src/Assignment.java
@@ -0,0 +1,18 @@
+/**
+ * @author Wallace Mooncai
+ * @since 10/1/2012
+ * 
+ * BMNA Week 5 Homework
+ *
+ */
+public class Assignment {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
